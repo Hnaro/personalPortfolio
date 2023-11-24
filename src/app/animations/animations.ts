7f4f67ffec7731c82514164679802cab95a641ea
Hnaro <arhonprincipe16@gmail.com>
2023-11-23 20:07:28 -0700

    header darklighticon animation applied

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,15 +1,34 @@
-import { animate, transition, trigger, state, stagger, style } from "@angular/animations";
-
+import { animate, transition, trigger, state, stagger, style, keyframes } from "@angular/animations";
 
 export const fadetextDarkMode = trigger("fadetextDarkMode", [
   state('dark', style({ "color":"#FFFFFF" })),
-  transition("* => dark", animate("200ms")),
-  transition("dark => *", animate("200ms"))
+  transition("* => dark", animate("100ms")),
+  transition("dark => *", animate("100ms"))
 ]);
 export const fadeBGDarkMode = trigger("fadeBGDarkMode", [
   state('dark', style({ "background-color":"var(--darkModeBG)" })),
-  transition("* => dark", animate("200ms")),
-  transition("dark => *", animate("200ms"))
+  transition("* => dark", animate("100ms")),
+  transition("dark => *", animate("100ms"))
 ]);
-
+export const slideLeftLightModeIcon = trigger("slideLeftLightModeIcon", [
+  transition(":enter", [
+    style({ opacity: 0, transform: "translateX(-200%)" }), //apply default styles before animation starts
+    animate(
+        "750ms ease-in-out",
+        style({ opacity: 1, transform: "translateX(0)" })
+    )
+])
+]);
+export const slideRightLightModeIcon = trigger("slideRightLightModeIcon", [
+  transition("* => *", [
+    style({ opacity: 0, transform: "translateX(200%)" }), //apply default styles before animation starts
+    animate(
+        "750ms ease-in-out",
+        style({ opacity: 1, transform: "translateX(0)" })
+    )
+])
+]);
+/* export const slideRightDarkModeIcon = trigger("slideRightDarkModeIcon", [
+  state("", style({ "transform":"translateX(5px)" })),
+]); */
 //export const fadeInOutDarkModeIcon = trigger();
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,40 +1,55 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { fadetextDarkMode } from 'src/app/animations/animations';
+import { fadetextDarkMode, slideLeftLightModeIcon, slideRightLightModeIcon } from 'src/app/animations/animations';
 import { MainServiceService } from 'src/app/services/main-service.service';
 
 @Component({
   selector: 'app-header',
-  animations: [fadetextDarkMode],
+  animations: [fadetextDarkMode, slideLeftLightModeIcon, slideRightLightModeIcon],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
   // default icon
-  lightModeIcon = "../../../assets/images/lightIcon.jpg"
-  src = "";
+  lightModeIcon = "../../../assets/images/lightIcon.jpg";
+  // logo
+  ASLogo: any;
   colorMode: any;
+  src: any;
   // default BGcontainer for icon
   isDefaultModeOn: any;
-
+  isSlideLeftAnimDone: any;
   constructor(private themeService: MainServiceService) {}
 
   ngOnInit(): void {
     // default
      this.src = this.lightModeIcon;
-     this.colorMode = "light";
-     this.isDefaultModeOn = this.themeService.getisDefaultThemeOn();
+     this.ASLogo = "../../../assets/images/ASLOGOLight.jpg";
+     this.colorMode = 'light';
+     this.isDefaultModeOn = true;
+     this.isSlideLeftAnimDone = false;
   }
   // change theme
   onChangeTheme() {
     if (this.themeService.getisDefaultThemeOn()) {
       this.colorMode = "dark";
+      this.ASLogo = "../../../assets/images/ASLOGODark.jpg";
       this.themeService.setThemeState(false);
+      this.isDefaultModeOn = false;
+      let i = setTimeout(() => {
+        this.isSlideLeftAnimDone = true;
+        clearTimeout(i);
+      }, 50);
     } else {
       this.colorMode = "light";
+      this.ASLogo = "../../../assets/images/ASLOGOLight.jpg";
       this.themeService.setThemeState(true);
+      this.isSlideLeftAnimDone = false;
+      let i = setTimeout(() => {
+        this.isDefaultModeOn = true;
+        clearTimeout(i);
+      }, 50);
     }
-    this.isDefaultModeOn = this.themeService.getisDefaultThemeOn();
     // sets the dark mode on to true or false to reset to default theme
   }
 }
